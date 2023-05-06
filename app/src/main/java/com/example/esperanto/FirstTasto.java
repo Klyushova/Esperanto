f863b0f26db2308bc8a48db6f0788c110c9a03b5
Klyushova <aklyushova@yandex.ru>
2023-05-06 13:27:36 +0700

    loman nado fix

--- app/src/main/java/com/example/esperanto/FirstTasto.java
+++ app/src/main/java/com/example/esperanto/FirstTasto.java
@@ -7,6 +7,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 
 import androidx.annotation.Nullable;
@@ -18,15 +20,28 @@ import java.util.Random;
 public class FirstTasto extends Grammari {
     private int TrueResult = 0;
     private FirstTastoBinding binding;
+    private int j  = 0;
     private String randomC[] = {"patro", "bovo", "koko", "kokoj", "studentoj"};
     private String randomG[] = {"iris", "ami", "estos", "volas", "vivi"};
     private String randomN[] = {"ofte", "sole", "zorgeme", "rapide", "tre"};
     private String randomP[] = {"griza", "longa", "alta", "ronda", "amara"};
-    private LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
-            LinearLayout.LayoutParams.MATCH_PARENT,
-            LinearLayout.LayoutParams.WRAP_CONTENT);
+
+    private String NotLa[] = {"_ Kopenhago", "_ sinjoro", "_ novembro", "_ Tiu ĉambro", "_ Petro"};
+    private String La[] = {"_ itala lingvo", "_ nova domo", "_ bela juna knabo", " Ludoviko _ dek kvara", "Vilgelmo _ Silentema"};
+
+    private String Vopross[][] = {{"_ li estas?", "Kie"}, {"_ estas ĉi tio?", "Kio"}, {"_ vi estas malgaja?", "Kial"},
+            {"_ vi deziras ludi?", "Kiel"}, {"_ vi povas helpi min?", "ĉu"}, {"_ kostas ĉi tio?", "Kiom"},
+            {"_ vi loĝas?", "Kie"}, {"_ horo estas?", "Kioma"}, {"_ vi havas mian libron?", "ĉu"}, {"_ granda ĝi estas?", "kiel"}};
+    private String mest[][]= {{"я", "mi"}, {"ты", "vi"}, {"ты", "вы"}, {"она", "ŝi"}, {"они", "ili"},
+            {"их", "ilia"}, {"наш", "nia"}, {"его", "lia"}, {"мой", "mia"}, {"он", "li"}, {"мы", "ni"}};
+
+    private String Predl[] = {"sub", "inter", "en", "ekster", "apud", "antau", "al", "kun", "dum", "el"};
+
    private int randompatro;
    private int t = 0;
+   private EditText editVo;
+   private Button btprover;
+   private String answerVo;
     public static Intent newInstance(Context context) {
         return new Intent(context, FirstTasto.class);
     }
@@ -40,83 +55,255 @@ public class FirstTasto extends Grammari {
             TastojPartoj();
             onChastiR();
         }
+        else  if(i == "la"){
+            TascoLa();
+            laOrNot();
+        }
+        else if(i == "vopross"){
+            TascoVopr();
+            Vopro();
+        }
+        else if(i == "mestoim"){
+            Mest();
+        }
+        else if(i == "predl"){
+            Pred();
+
+        }
 
 
     }
-    private void TastojPartoj(){
+      private void TastojPartoj(){
+           t +=1;
+           if (t > 10){
+               binding.lB.setVisibility(View.GONE);
+               binding.answertasto.setText("Урок пройден");
+           }
+           else {
+               Random random = new Random();
+               randompatro = random.nextInt(4 - 1) + 1;
+               int randomWord = random.nextInt(4 - 0) + 0;
+               switch (randompatro) {
+                   case 1:
+                       binding.answertasto.setText(valueOf(randomG[randomWord]));
+                       break;
+                   case 2:
+                       binding.answertasto.setText(valueOf(randomC[randomWord]));
+                       break;
+                   case 3:
+                       binding.answertasto.setText(valueOf(randomP[randomWord]));
+                       break;
+                   default:
+                       binding.answertasto.setText(valueOf(randomN[randomWord]));
+                       break;
+               }
+           }
+
+       }
+      private void onChastiR(){
+          LinearLayout ll = (LinearLayout)findViewById(R.id.lB);
+          LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                  LinearLayout.LayoutParams.MATCH_PARENT,
+                  LinearLayout.LayoutParams.WRAP_CONTENT);
+           Button button = new Button(this);
+           button.setText("глагол");
+           Button buttonN = new Button(this);
+           buttonN.setText("наречие");
+           Button buttonP = new Button(this);
+           buttonP.setText("прилагательное");
+           Button buttonC = new Button(this);
+           buttonC.setText("существительное");
+           ll.addView(button, params);
+           ll.addView(buttonC, params);
+           ll.addView(buttonN, params);
+           ll.addView(buttonP, params);
+
+           button.setOnClickListener(view -> {
+               if(randompatro == 1) {
+                       TrueResult += 1;
+                       binding.result.setText("" + TrueResult + "/ 10");
+                   }
+               TastojPartoj();
+           });
+           buttonC.setOnClickListener(view -> {
+               if(randompatro == 2) {
+                   TrueResult += 1;
+                   binding.result.setText("" + TrueResult + "/ 10");
+               }
+               TastojPartoj();
+           });
+           buttonN.setOnClickListener(view -> {
+               if(randompatro == 3) {
+                   TrueResult += 1;
+                   binding.result.setText("" + TrueResult + "/ 10");
+               }
+               TastojPartoj();
+           });
+
+           buttonP.setOnClickListener(view -> {
+               if(randompatro == 4) {
+                   TrueResult += 1;
+                   binding.result.setText("" + TrueResult + "/ 10");
+               }
+               TastojPartoj();
+           });
+       }
+    private void TascoLa(){
         t +=1;
         if (t > 10){
             binding.lB.setVisibility(View.GONE);
             binding.answertasto.setText("Урок пройден");
-
-
         }
         else {
             Random random = new Random();
-            randompatro = random.nextInt(4 - 1) + 1;
+            randompatro = random.nextInt(2 - 1) + 1;
             int randomWord = random.nextInt(4 - 0) + 0;
             switch (randompatro) {
                 case 1:
-                    binding.answertasto.setText(valueOf(randomG[randomWord]));
+                    binding.answertasto.setText(valueOf(La[randomWord]));
                     break;
                 case 2:
-                    binding.answertasto.setText(valueOf(randomC[randomWord]));
-                    break;
-                case 3:
-                    binding.answertasto.setText(valueOf(randomP[randomWord]));
-                    break;
-                default:
-                    binding.answertasto.setText(valueOf(randomN[randomWord]));
+                    binding.answertasto.setText(valueOf(NotLa[randomWord]));
                     break;
             }
+        }
+    }
+
+    private void laOrNot(){
+        LinearLayout ll = (LinearLayout)findViewById(R.id.lB);
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT);
+        Button butLa = new Button(this);
+        butLa.setText("la");
+        Button butNotla = new Button(this);
+        butNotla.setText("not la");
+        ll.addView(butLa, params);
+        ll.addView(butNotla, params);
+        butLa.setOnClickListener(view -> {
+            if (randompatro == 1){
+                TrueResult += 1;
+                binding.result.setText("" + TrueResult + "/ 10");
+            }
+            TascoLa();
+        });
+        butNotla.setOnClickListener(view -> {
+            if (randompatro == 2){
+                TrueResult += 1;
+                binding.result.setText("" + TrueResult + "/ 10");
+            }
+            TascoLa();
+        });
+    }
+
+    private  void TascoVopr(){
+        t +=1;
+        if (t > 10){
+            binding.lB.setVisibility(View.GONE);
+            binding.answertasto.setText("Урок пройден");
+        }
+        else {
+            Random random = new Random();
+            int randomWord = random.nextInt(9 - 0) + 0;
+            binding.answertasto.setText(valueOf(Vopross[randomWord][0]));
+            answerVo = Vopross[randomWord][1];
+
 
         }
-
     }
-    public void onChastiR(){
-        Button button = new Button(this);
-        button.setText("глагол");
-        Button buttonN = new Button(this);
-        buttonN.setText("наречие");
-        Button buttonP = new Button(this);
-        buttonP.setText("прилагательное");
-        Button buttonC = new Button(this);
-        buttonC.setText("существительное");
+    private void Vopro(){
         LinearLayout ll = (LinearLayout)findViewById(R.id.lB);
-        ll.addView(button, params);
-        ll.addView(buttonC, params);
-        ll.addView(buttonN, params);
-        ll.addView(buttonP, params);
-
-        button.setOnClickListener(view -> {
-            if(randompatro == 1) {
-                    TrueResult += 1;
-                    binding.result.setText("" + TrueResult + "/ 10");
-                }
-            TastojPartoj();
-        });
-        buttonC.setOnClickListener(view -> {
-            if(randompatro == 2) {
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT);
+        EditText editVo = new EditText(this);
+        ll.addView(editVo, params);
+        Button btprover = new Button(this);
+        btprover.setText("Проверить");
+        ll.addView(btprover, params);
+        btprover.setOnClickListener(view -> {
+            String editi = editVo.getText().toString();
+            if(editi.equals(answerVo)){
                 TrueResult += 1;
-                binding.result.setText("" + TrueResult + "/ 10");
             }
-            TastojPartoj();
-        });
-        buttonN.setOnClickListener(view -> {
-            if(randompatro == 3) {
-                TrueResult += 1;
-                binding.result.setText("" + TrueResult + "/ 10");
-            }
-            TastojPartoj();
+            editVo.getText().clear();
+            TascoVopr();
         });
 
-        buttonP.setOnClickListener(view -> {
-            if(randompatro == 4) {
+
+
+    }
+    private void inLay(){
+        LinearLayout ll = (LinearLayout)findViewById(R.id.lB);
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT);
+        EditText editVo = new EditText(this);
+        ll.addView(editVo, params);
+        Button btprover = new Button(this);
+        btprover.setText("Проверить");
+        ll.addView(btprover, params);
+        btprover.setOnClickListener(view -> {
+            String editi = editVo.getText().toString();
+            if(editi.equals(answerVo)){
                 TrueResult += 1;
-                binding.result.setText("" + TrueResult + "/ 10");
             }
-            TastojPartoj();
+            editVo.getText().clear();
         });
     }
+
+    private void Mest(){
+        inLay();
+        btprover.setOnClickListener(view -> {
+            String editi = editVo.getText().toString();
+            if(editi.equals(answerVo)){
+                TrueResult += 1;
+            }
+            editVo.getText().clear();
+        });
+
+
+    }
+
+    private void Pred(){
+        LinearLayout ll = (LinearLayout)findViewById(R.id.lB);
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT);
+        ImageView imageView = new ImageView(this);
+        ll.addView(imageView, params);
+        EditText editVo = new EditText(this);
+        ll.addView(editVo, params);
+        Button btprover = new Button(this);
+        btprover.setText("Проверить");
+        ll.addView(btprover, params);
+        int resID = getResources().getIdentifier(Predl[8], "drawable", getPackageName());
+        imageView.setImageResource(resID);
+        btprover.setOnClickListener(view -> {
+            String editi = editVo.getText().toString();
+            if(editi.equals(answerVo)){
+                TrueResult += 1;
+                Nopred();
+
+            }
+            editVo.getText().clear();
+        });
+
+
+    }
+    private void Nopred(){
+        t ++;
+        if (t > 10){
+            binding.lB.setVisibility(View.GONE);
+            binding.answertasto.setText("Урок пройден");
+        }
+        else{
+            j ++;
+            Pred();
+        }
+    }
+
+
+
 
 }
